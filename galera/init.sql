266555b927903409c04c1933662c288fa57dec7f
Rullabcde <choirulrasyid09@gmail.com>
2025-06-23 13:50:15 +0700

    update init.sql

--- galera/init.sql
+++ galera/init.sql
@@ -1,7 +1,7 @@
 CREATE USER 'rullabcd'@'%' IDENTIFIED BY 'rullabcd';
 GRANT ALL PRIVILEGES ON *.* TO 'rullabcd'@'%' WITH GRANT OPTION;
 
-CREATE USER 'monitor'@'%' IDENTIFIED BY 'monitorpass';
+CREATE USER 'monitor'@'%' IDENTIFIED BY 'monitor';
 GRANT USAGE ON *.* TO 'monitor'@'%';
 GRANT SELECT ON mysql.user TO 'monitor'@'%';
 
--- proxysql/init.sql
+++ proxysql/init.sql
@@ -13,7 +13,7 @@ INSERT INTO mysql_users (username, password, default_hostgroup) VALUES
 
 -- User monitoring
 INSERT INTO mysql_users (username, password, default_hostgroup, transaction_persistent) VALUES
-('monitor', 'monitorpass', 10, 1);
+('monitor', 'monitor', 10, 1);
 
 -- Query rules
 INSERT INTO mysql_query_rules (rule_id, active, match_pattern, destination_hostgroup, apply) VALUES
