c333c411885110908563ae68b4d12a5e56b5d938
Rullabcde <choirulrasyid09@gmail.com>
2025-06-23 10:24:41 +0700

    first commit

---
+++ proxysql/init.sql
@@ -0,0 +1,15 @@
+DELETE FROM mysql_servers;
+INSERT INTO mysql_servers (hostgroup_id, hostname, port) VALUES
+(10, 'db1', 3306),
+(10, 'db2', 3306),
+(10, 'db3', 3306);
+
+DELETE FROM mysql_users;
+INSERT INTO mysql_users (username, password, default_hostgroup) VALUES
+('rullabcd', 'rullabcd', 10);
+
+LOAD MYSQL SERVERS TO RUNTIME;
+LOAD MYSQL USERS TO RUNTIME;
+
+SAVE MYSQL SERVERS TO DISK;
+SAVE MYSQL USERS TO DISK;
